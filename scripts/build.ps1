96fb7a011e90034a9347ef51cca2f39719e933cc
vzze <lolo32778@gmail.com>
2023-02-19 18:09:23 +0200

    update scripts

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,21 +1,10 @@
-param(
-    [Parameter()]
-    [Boolean]$BuildFell = $True,
-    [Boolean]$BuildTests = $True
-)
-
 $check_dir = Test-Path ./build -PathType Container
 
 if($check_dir -eq $false) {
     New-Item build -ItemType Directory
 }
 
-Write-Host "`n-------------- OPTS --------------`n"
-
-Write-Host "BuildFell:  $BuildFell"
-Write-Host "BuildTests: $BuildTests"
-
 Write-Host "`n-------------- CMAKE --------------`n"
 
-Invoke-Expression "cmake -S . -B build -G `"MinGW Makefiles`" -DBUILD_TESTS=$BuildTests -DBUILD_FELL=$BuildFell"
+Invoke-Expression "cmake -S . -B build -G `"MinGW Makefiles`""
 Invoke-Expression "cmake --build ./build"
